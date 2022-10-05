a9416db95ee2f38dbae93d6f7280678949adfe24
ninaro1966 <ovchinnikova@specmont.ru>
2022-10-05 22:58:31 +0300

    task4 commited

---
+++ HelloPython/task4.py
@@ -0,0 +1,8 @@
+# 4.Реализуйте алгоритм перемешивания списка, без использования встроеных методов (особенно SHUFFLE,
+# без него) можно (нужно) использовать библиотеку Random
+
+professions = ['Journalist', 'Economist', 'IT specialist']
+print(f'Введены мои профессии: {professions}')
+professions.reverse()
+print(f'Перетряхнув список в обратном порядке, получилось: {professions}')
+
